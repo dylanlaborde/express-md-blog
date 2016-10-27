81aa803d3ab7d9836db461a53f9b2f247457e9f5
LABORDE <dylan4047@gmail.com>
2016-10-27 13:54:38 +0200

    add public folder

---
+++ public/js/app.js
@@ -0,0 +1,56 @@
+(function(){
+	"use strict";
+	var app = {
+		menuJson : $.ajax("http://192.168.1.113:1337/menu.json"),
+		
+		init:function(){
+			app.listener();
+		},
+		markdone: function(){
+			var markdown = $.ajax("http://192.168.1.113:1337/example.md")
+			markdown.done(function(){
+				$.get("http://192.168.1.113:1337/example.md",function(data){
+					var converter = new showdown.Converter();
+					var text = data;
+					var html = converter.makeHtml(text);
+					$("#md").append(html);
+				});
+			});
+		},
+		listener : function(){
+			app.menuJson.done(function(){
+				app.menuDone();
+			});
+		},
+		menuDone : function(){
+			app.menuJson.done(function(menuJ){
+				for (var i = 0; i < menuJ.menu.length; i++) {
+					menuJ[i]
+
+					var pathJson = "http://192.168.1.113:1337"+menuJ.menu[i].path;
+					var titleJson = menuJ.menu[i].title;
+					$("#menu").append("<span>"+"<a class='mark' data-number="+pathJson+" href=''>"+titleJson+"</a>"+"</span>");
+					app.convert();
+				}
+			})
+		},
+		convert : function(){
+			$('.mark').click(function(){
+				event.preventDefault();
+				app.data($(this).data('number'));
+			});
+		},
+		data: function(path){
+			$.ajax(path, function(){
+			}).done(function(response){
+				var converter = new showdown.Converter();
+				var text = response;
+				var html = converter.makeHtml(text);
+				$("#md").html(html);		
+			});
+		},
+	};
+	$(document).ready(function(){
+		app.init();
+	});
+})();
